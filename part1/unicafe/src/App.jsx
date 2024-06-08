7d1929f6d4b1d04dd09d15554a7fddbb9216063c
scriptwes <scriptwes@gmail.com>
2024-06-09 00:10:01 +0800

    Completed exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -26,6 +26,9 @@ function App() {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average {(good - bad) / (good + neutral + bad)}</p>
+      <p>positive {good / (good + neutral + bad)}%</p>
     </>
   );
 }
