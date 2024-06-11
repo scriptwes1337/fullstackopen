ce5f9740077a4ca4d1a902002a8896e1b15b4e41
scriptwes <scriptwes@gmail.com>
2024-06-11 20:09:37 +0800

    Completed exercise 4.4

--- part4/blog/tests/list_helper_test.js
+++ part4/blog/tests/list_helper_test.js
@@ -2,9 +2,24 @@ const { test, describe } = require("node:test");
 const assert = require("node:assert");
 const listHelper = require("../utils/list_helper");
 
-test("Dummy always returns one", () => {
-  const blogs = [];
+describe("Exercise 4.3", () => {
+  test("Dummy always returns one", () => {
+    const blogs = [];
 
-  const result = listHelper.dummy(blogs);
-  assert.strictEqual(result, 1);
+    const result = listHelper.dummy(blogs);
+    assert.strictEqual(result, 1);
+  });
+});
+
+describe("Exercise 4.4", () => {
+  const blogWithOneLike = [
+    {
+      likes: 1,
+    },
+  ];
+
+  test("Find total likes - When list has only one blog with one like, equals the likes of that", () => {
+    const result = listHelper.totalLikes(blogWithOneLike);
+    assert.strictEqual(result, 1);
+  });
 });
