a2cfc29c86a534383d9a90c8ef46a156292aeef7
scriptwes <scriptwes@gmail.com>
2024-06-14 15:08:08 +0800

    Completed exercise 5.8

--- README.md
+++ README.md
@@ -105,7 +105,7 @@ Asterisk denotes an optional exercise
 - [x] 5.5
 - [x] 5.6
 - [x] 5.7
-- [ ] 5.8
+- [x] 5.8
 - [ ] 5.9
 - [ ] 5.10
 - [ ] 5.11
--- part5/blog/frontend/src/components/Blog.jsx
+++ part5/blog/frontend/src/components/Blog.jsx
@@ -1,6 +1,8 @@
+import axios from "axios";
 import React, { useState } from "react";
 
 export const Blog = ({ blog }) => {
+  const [blogData, setBlogData] = useState(blog);
   const [viewBlog, setViewBlog] = useState(false);
   const [buttonLabel, setButtonLabel] = useState("view");
 
@@ -13,21 +15,34 @@ export const Blog = ({ blog }) => {
     }
   };
 
+  const handleLike = async () => {
+    try {
+     blogData.likes += 1
+      await axios.put(`/api/blogs/${blogData.id}`, blogData);
+      setBlogData({...blogData});
+    } catch (err) {
+      console.log("Like unsuccessful", err.message);
+    }
+  };
+
   return (
     <div>
       {viewBlog ? (
         <div style={{ border: "1px solid black" }}>
           <p>
-            Title: {blog.title}{" "}
+            Title: {blogData.title}{" "}
             <button onClick={handleView}>{buttonLabel}</button>
           </p>{" "}
-          <p>Url: {blog.url}</p> 
-          <p>Likes: {blog.likes}</p>{" "}
-          <p>Author: {blog.author}</p>
+          <p>Url: {blogData.url}</p>
+          <p>
+            Likes: {blogData.likes}
+            <button onClick={handleLike}>like</button>
+          </p>
+          <p>Author: {blogData.author}</p>
         </div>
       ) : (
         <div>
-          {blog.title} {blog.author}
+          {blogData.title} {blogData.author}
           <button onClick={handleView}>{buttonLabel}</button>
         </div>
       )}
